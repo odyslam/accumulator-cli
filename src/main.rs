5e24517438d10fa4406e3431390b175fe24192a3
Odysseas.eth <odyslam@gmail.com>
2022-05-13 15:09:10 +0300

    chore: add readme, license
    WIP: simplifaction, garbage output still
    
    feat: correct output for arbitrary string
    
    chore: add comments
    
    chore: add package info
    
    feat: error handling; debug; multiple messages
    
    chore: add help messages, remove mood libs
    
    chore: add version dependency to accumulator
    
    chore: replace tree with NomadTree

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,82 @@
-use std::str::FromStr;
-
-use accumulator::{Tree, Proof, Merkle, MerkleProof};
-use ethers::abi::token::Tokenizer;
-use ethers::abi::{decode, encode, token, ParamType, param_type, Token, FixedBytes, Tokenize};
-use ethers::types;
-use primitive_types::H256;
+use accumulator::{utils::hash, Merkle, MerkleProof, NomadTree};
+use anyhow::{Context, Result, anyhow};
 use clap::Parser;
-use std::str;
-
-
+use ethers::abi::{AbiEncode};
+use ethers::types::*;
+use rustc_hex::ToHex;
 
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[clap(author, version, about)]
 struct Args {
-    /// Name of the person to greet
     #[clap(short, long)]
-    message: String,
-
-    /// Number of times to greet
-    #[clap(short, long, default_value_t = 1)]
-    count: u8,
-    #[clap(parse(from_flag))]
-    root: bool,
-    #[clap(parse(from_flag))]
-    proof: bool
+    /// The string to be added to the tree. Multiple can be supplied
+    message: Vec<String>,
+    #[clap(short, long)]
+    /// The index of the leaf for which the proof is generated
+    index: usize,
+    #[clap(short, long)]
+    /// Print Debug information
+    debug: bool,
 }
 
-
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
-    let mut tree: Tree<32> = Default::default();
-    tree.ingest(H256::from_str(&args.message).unwrap()).unwrap();
-    let proof = tree.prove(0).unwrap();
-    let leaf = proof.leaf.to_string();
-    let index = proof.index.to_string();
-    let path = proof.path.map(|hash| hash.to_string());
-    let token3 = Token::FixedBytes(path.iter().map(|hash| hash.as_bytes()).collect<Vec<u8>>());
-    let tokens = [Tokenizer::tokenize(&ParamType::String, &leaf).unwrap(), Tokenizer::tokenize(&ParamType::Uint(256), &index).unwrap(), token3];
-    let encoded_output = str::from_utf8(&encode(&tokens)).unwrap();
-    println!("{encoded_output}");
+    // Create a Sparse Merkle Tree with depth 32
+    // Insert the message to the tree
+    let messages = args.message;
+    let mut tree = NomadTree::default();
+    if messages.len() == 0 {
+        return Err(anyhow!("You need to supply at least a message to be inserted in the tree\nType accumulator-cli -h for help"));
+    }
+    for message in &messages {
+        tree.ingest(hash(message)).context(format!("Accumulator can't ingest {}", message))?;
+    }
+    // Calculate the Proof struct for the message at index 0
+    let index = args.index;
+    let proof = tree.prove(index).context(format!(
+        "Accumulator can't generate proof for message at index {}",
+        index
+    ))?;
+    let leaf = proof.leaf.to_fixed_bytes();
+    let root = proof.root().to_fixed_bytes();
+    let index: U256 = proof.index.into();
+    // The path is an array of 32 hashes as FixedBytes (bytes32 in solidity).
+    let path = proof.path.map(|hash| hash.to_fixed_bytes());
+    // Abi-Encode the data
+    // For quick reference, here is how Rust types are abi-encoded to Solidity types.
+    // [u8; X] -> bytesX
+    // Vec<u8> -> bytes (dynamic)
+    // String  -> string (dynamic)
+    // Bool    -> bool
+    // u64     -> uint64 (etc. for other numbers)
+    let encoded = (root, leaf, index, path).encode().to_hex::<String>();
+    if args.debug {
+        println!(
+            r#"
+//////////////////////////////////////////////////////////////
+                           DEBUG INFO
+//////////////////////////////////////////////////////////////
+"#
+        );
+        println!(
+            "🌴TREE\nMessages: {:?}\nRoot: {}\n\nPROOF\nRequested Leaf: {}\nRequested Index: {}\nPath: \n{}",
+            messages,
+            root.to_hex::<String>(),
+            leaf.to_hex::<String>(),
+            index,
+            path.into_iter()
+                .enumerate()
+                .map(|(i, x)| format!("{:02}: {}\n", i, x.to_hex::<String>()))
+                .collect::<String>()
+        );
+        println!(
+            r#"
+//////////////////////////////////////////////////////////////
+                       ABI-ENCODED OUTPUT
+//////////////////////////////////////////////////////////////
+"#
+        );
+    }
+    println!("{encoded}");
+    Ok(())
 }
