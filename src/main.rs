d35add165ce1679da2ce9591eb451ccbfc5952ab
Odysseas.eth <odyslam@gmail.com>
2022-05-17 23:08:15 +0300

    chore: add comments

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,23 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
+    // Create a Sparse Merkle Tree with depth 32
     let mut tree: Tree<32> = Default::default();
+    // Insert the message to the tree
     tree.ingest(hash(args.message)).unwrap();
+    // Calculate the Proof struct for the message at index 0
     let proof = tree.prove(0).unwrap();
     let leaf = proof.leaf.to_fixed_bytes();
     let index: U256 = proof.index.into();
+    // The path is an array of 32 hashes as FixedBytes (bytes32 in solidity).
     let path = proof.path.map(|hash| hash.to_fixed_bytes());
+    // Abi-Encode the data.
+    // For quick reference, here is how Rust types are abi-encoded to Solidity types.
+    // [u8; X] -> bytesX
+    // Vec<u8> -> bytes (dynamic)
+    // String -> string (dynamic)
+    // bool -> bool
+    // u64 -> uint64 (etc. for other numbers)
     let encoded = (leaf, index, path).encode().to_hex::<String>();
     println!("{encoded}");
 }
